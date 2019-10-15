0a3166422a3faa536addcc99ef6adb35320b1b11
0273898 <0273898@KELCE-LABS13-P.pittstate.edu>
2019-10-15 12:11:36 -0500

    end of class

---
+++ src/main/java/UserDemonstration.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0273898
+ */
+public class UserDemonstration {
+    public static void main(String[] args) {
+        
+        User myUser = new User("johnk@test.com", "johnk", "password", 5);
+        
+        
+        
+        
+    
+        
+        
+    }
+}
